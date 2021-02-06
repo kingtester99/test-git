12bb4484b23ff5492763ca9e6a92ca78301f82d6
kingtester <KingTester99@gmail.com>
2021-02-06 12:21:45 +0800

    [Tester] add new porperty and method in "llh_test"

--- llh_test.java
+++ llh_test.java
@@ -8,4 +8,14 @@ public class llh_test {
 	public boolean runCmdOfLlh(String cmd) {
 		return true;
 	}
+	
+	private String mode = null;
+	
+	public void setterMode(String mode) {
+		this.mode = mode;
+	}
+	
+	public String getterMode() {
+		return this.mode;
+	}
 }
