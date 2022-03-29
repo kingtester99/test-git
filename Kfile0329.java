1707262e0b26adf0469914d7abfcdf03d29d3f64
KingTester <KingTester99@gmail.com>
2022-03-29 15:48:24 +0800

    Test scenario 3 by "K"
    update file "Kfile0329" again

--- Kfile0329.java
+++ Kfile0329.java
@@ -4,5 +4,7 @@ public class Kfile0329 {
 	
 	public static void main (String args[]) {
 		System.out.println("Scenario 2 : K update file")
+		
+		System.out.println("Scenario 3 >>> K update file again")
 	}
 }
