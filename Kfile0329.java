e624e494201d5c33aa5ccae62769dd3c0bb87f95
KingTester <KingTester99@gmail.com>
2022-03-29 15:13:44 +0800

    Test Scenario 2 by "K"
    update file "Kfile0329"

--- Kfile0329.java
+++ Kfile0329.java
@@ -1,6 +1,8 @@
 public class Kfile0329 {
 	public static final String filename = "K-file0329";
 	
+	
 	public static void main (String args[]) {
+		System.out.println("Scenario 2 : K update file")
 	}
 }
