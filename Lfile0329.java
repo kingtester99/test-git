7df756937329316b79da63cdef8299bef3d2b882
KingTester <KingTester99@gmail.com>
2022-03-30 14:09:11 +0800

    Hotfix#24 fix bug
    update Lfile0329 for fixing bug

--- Lfile0329.java
+++ Lfile0329.java
@@ -2,5 +2,6 @@ public class Lfile0329 {
 	public static final String filename = "Lfile0329";
 	
 	public static void main(String args[]) {
+		System.out.println("update bug for Hotfix24 by 'K' ");
 	}
 }
