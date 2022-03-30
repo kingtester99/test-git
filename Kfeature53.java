fcf8424d6eb8bcf99ac660b83a59f9c2a6914de2
KingTester <KingTester99@gmail.com>
2022-03-30 13:46:55 +0800

    Feature#53 modify REQ.
    update Kfeature53.java due to CR

--- Kfeature53.java
+++ Kfeature53.java
@@ -3,5 +3,7 @@ public class Kfeature53 {
 	
 	public static void main(String[] args) {
 		System.out.println("do Feature #53 by 'K'");
+		
+		System.out.println("do Feature #53 by 'K' on 03/30 13:40");		
 	}
 }
