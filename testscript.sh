b933083ffafde7c6fb4848ce61d4ca56d4bdf83c
April <April@AprildeMacBook-Air.local>
2022-12-26 18:25:43 +0800

    updated at 18:25

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,6 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "created in 12-26 03:38"
 echo "updated in 12-26 18:09"
-echo "updated in 12-26 18:20" 
+echo "updated in 12-26 18:20"
+echo "updated in 12-26 18:25" 
+ 
